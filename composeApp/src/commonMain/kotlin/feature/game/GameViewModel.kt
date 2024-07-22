0167c866e69ba62a7e899356f9722e2101d4801d
Alexis Aneas <alexis.aneas@outlook.fr>
2024-07-22 23:46:49 +0200

    create a BaseViewModel to easily retrieve preferences across every ViewModels

--- composeApp/src/commonMain/kotlin/feature/game/GameViewModel.kt
+++ composeApp/src/commonMain/kotlin/feature/game/GameViewModel.kt
@@ -1,42 +1,29 @@
 package feature.game
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import data.model.Grid
-import data.model.Pixel
 import data.model.Map
-import data.repository.AppPreferences
-import data.repository.PreferenceRepository
-import kotlinx.coroutines.Job
+import data.model.Pixel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
+import ui.BaseViewModel
 
-class GameViewModel(
-    private val preferenceRepository: PreferenceRepository
-): ViewModel() {
+class GameViewModel: BaseViewModel() {
 
     private val _state = MutableStateFlow(GameState())
     val state = _state.asStateFlow()
 
-    private val _preference = MutableStateFlow(AppPreferences())
-    val preferences = _preference.asStateFlow()
-
-    private var preferencesJob: Job? = null
-
     private var currentPlayer = 0
 
     init {
-        listenPreferences()
-        _state.update { it.copy(pixelNumber = _preference.value.pixelNumber) }
+        _state.update { it.copy(pixelNumber = preferences.value.pixelNumber) }
         when (_state.value.pixelNumber) {
             4 -> _state.update { it.copy(pixelSet = Pixel.COLOR_SET_4.toTypedArray()) }
             5 -> _state.update { it.copy(pixelSet = Pixel.COLOR_SET_5.toTypedArray()) }
             6 -> _state.update { it.copy(pixelSet = Pixel.COLOR_SET_6.toTypedArray()) }
             7 -> _state.update { it.copy(pixelSet = Pixel.COLOR_SET_7.toTypedArray()) }
         }
-        val grid = Grid(_preference.value.gridX, _preference.value.gridY)
+        val grid = Grid(preferences.value.gridX, preferences.value.gridY)
         _state.update { it.copy(map = Map.generateMap(grid, _state.value.pixelNumber), player = currentPlayer) }
         _state.update { it.copy(playersPixels = _state.value.map.getPlayersPixel()) }
     }
@@ -55,14 +42,6 @@ class GameViewModel(
                     _state.update { it.copy(endGame = EndGame(player0, player1)) }
                 }
             }
-        }
-    }
-
-    private fun listenPreferences() {
-        preferencesJob?.cancel()
-        preferencesJob = viewModelScope.launch {
-            preferenceRepository.preferences.collect { preferences ->
-                _preference.update { preferences } }
         }
     }
 
--- composeApp/src/commonMain/kotlin/feature/settings/SettingsViewModel.kt
+++ composeApp/src/commonMain/kotlin/feature/settings/SettingsViewModel.kt
@@ -1,28 +1,16 @@
 package feature.settings
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.chapeaumoineau.pixelinvasion.feature.settings.SettingsEvent
-import data.repository.AppPreferences
 import data.repository.PreferenceRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import ui.BaseViewModel
 
 class SettingsViewModel(
     private val preferencesRepository: PreferenceRepository
-): ViewModel() {
-
-    private val _preference = MutableStateFlow(AppPreferences())
-    val preferences = _preference.asStateFlow()
-
-    private var preferencesJob: Job? = null
-
-    init { listenPreferences() }
+): BaseViewModel() {
 
     fun onEvent(event: SettingsEvent) {
         when (event) {
@@ -35,14 +23,6 @@ class SettingsViewModel(
             is SettingsEvent.ChangeGridY -> viewModelScope.launch(Dispatchers.IO) {
                 preferencesRepository.setGridY(event.value)
             }
-        }
-    }
-
-    private fun listenPreferences() {
-        preferencesJob?.cancel()
-        preferencesJob = viewModelScope.launch {
-            preferencesRepository.preferences.collect { preferences ->
-                _preference.update { preferences } }
         }
     }
 
